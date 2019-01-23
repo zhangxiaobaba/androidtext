2b79dff628f380cf9dd346d179a2a00503285ba5
苗胜杰 <2188065459@qq.com>
2019-01-23 13:49:38 +0800

    asdfghjkl;

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.administrator.myapplication;
 
+import android.app.NotificationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NotificationManager systemService = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
     }
 }
